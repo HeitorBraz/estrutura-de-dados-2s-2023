11e9f597c11e92a2130bfb6d519341b023bfdd5d
Heitor <heitor.reino@a.ucb.br>
2023-09-21 09:24:32 -0300

    Aula dia 18/09

---
+++ aula_1809/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+int main(int argc, char const *argv[]){
+    
+    Objeto o1;
+    Objeto o2;
+    Objeto* o;
+    o1.valor = 'A';
+    o2.valor = 'B'
+
+    
+    exit (0);
+}
---
+++ aula_1809/pilha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+Pilha pilha(){
+    Pilha* p;
+    p.topo = NULL;
+    p.quantidade = 0;
+    return p;
+}
+
+void empilhar(Objeto* o, Pilha* P){
+    o.proximo_objeto = P->topo;
+    P->topo = o;
+    P->quantidade++;
+}
+
+Objeto desempilhar(Pilha* P){
+    Objeto* o = P->topo
+    P->topo = o->proximo_objeto;
+    P->quantidade++;
+}
